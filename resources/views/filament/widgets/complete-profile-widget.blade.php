58a2d201e13f5c7a95b1837535d4f3ebe8c2b649
michalcecko <ceckomichal@gmail.com>
2026-04-22 19:23:44 +0200

    chore: update CompleteProfileWidget to include tenant-based profile edit URLs

--- resources/views/filament/widgets/complete-profile-widget.blade.php
+++ resources/views/filament/widgets/complete-profile-widget.blade.php
@@ -1,3 +1,9 @@
+@php
+    $tenant = \Filament\Facades\Filament::getTenant() ?? auth()->user()?->teams?->first();
+    $editUrl = $tenant
+        ? \App\Filament\Resources\Users\UserResource::getUrl('edit', ['record' => auth()->user(), 'tenant' => $tenant])
+        : null;
+@endphp
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="flex items-center gap-3">
@@ -10,14 +16,16 @@
                     Chýba vám telefón, dátum narodenia alebo pohlavie.
                 </p>
             </div>
-            <x-filament::button
-                :href="\App\Filament\Resources\Users\UserResource::getUrl('edit', ['record' => auth()->user()])"
-                tag="a"
-                size="sm"
-                color="warning"
-            >
-                Upraviť profil
-            </x-filament::button>
+            @if ($editUrl)
+                <x-filament::button
+                    :href="$editUrl"
+                    tag="a"
+                    size="sm"
+                    color="warning"
+                >
+                    Upraviť profil
+                </x-filament::button>
+            @endif
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
